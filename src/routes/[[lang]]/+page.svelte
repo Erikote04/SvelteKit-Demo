ad17d1f19dceda0e53b39d49de35a64f92b6ff60
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-31 20:53:59 +0100

    [Navigation] Default routes and params

---
+++ src/routes/[[lang]]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/stores';
+	const { lang = 'en' } = $page.params;
+	const greetings = {
+		en: 'Hello!',
+		es: '¡Hola!',
+		fr: 'Bonjour!'
+	};
+	const greeting = greetings[lang];
+</script>
+
+<h1>{greeting}</h1>
