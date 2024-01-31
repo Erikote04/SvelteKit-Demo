70f0eb9e2cdfbd7d14c135e495aecf30601b55e8
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-31 21:03:32 +0100

    [Navigation] Navigating from JavaScript

--- src/routes/[[lang]]/+page.svelte
+++ src/routes/[[lang]]/+page.svelte
@@ -1,16 +1,37 @@
 <script>
 	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+
 	const { lang = 'en' } = $page.params;
+
 	const greetings = {
 		en: 'Hello!',
 		es: '¡Hola!',
 		fr: 'Bonjour!'
 	};
+
 	const greeting = greetings[lang];
+
+	let placingOrder = false;
+
+	function handleClick() {
+		placingOrder = true;
+		setTimeout(() => {
+			placingOrder = false;
+			goto('/products');
+		}, 2000);
+	}
 </script>
 
-<h1>{greeting}</h1>
+{#if placingOrder}
+	<p>Placing order...</p>
+{:else}
+	<h1>{greeting}</h1>
 
-<a href="/blog">Blog</a>
-<a href="/docs">Docs</a>
-<a href="/products">Products</a>
+	<a href="/blog">Blog</a>
+	<a href="/docs">Docs</a>
+	<a href="/products">Products</a>
+
+	<p>Ready to order?</p>
+	<button on:click={handleClick}>Place order</button>
+{/if}
