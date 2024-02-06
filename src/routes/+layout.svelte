78d93934f5629606cc2c770ed17aae790a2d3db0
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-02-06 23:45:15 +0100

    [Loading] Showing a "loading..." message when navigating

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { page } from '$app/stores';
+	import { navigating, page } from '$app/stores';
 
 	import Footer from '$lib/Footer.svelte';
 	import Header from '$lib/Header.svelte';
@@ -7,17 +7,18 @@
 	export let data;
 </script>
 
-{#if $page.route.id !== '/(auth)/login' && $page.route.id !== '/(auth)/register'}
-	<Header />
-{/if}
-
-{#if $page.data.notification}
-	<p>{$page.data.notification}</p>
-{/if}
-
-Welcome {data.username}
-<slot />
-
-{#if $page.route.id !== '/(auth)/login' && $page.route.id !== '/(auth)/register'}
-	<Footer />
+{#if $navigating}
+	<div>loading...</div>
+{:else}
+	{#if $page.route.id !== '/(auth)/login' && $page.route.id !== '/(auth)/register'}
+		<Header />
+	{/if}
+	{#if $page.data.notification}
+		<p>{$page.data.notification}</p>
+	{/if}
+	welcome {data.username}
+	<slot />
+	{#if $page.route.id !== '/(auth)/login' && $page.route.id !== '/(auth)/register'}
+		<Footer />
+	{/if}
 {/if}
